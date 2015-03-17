51f3f6655c08d2f8c01cccffbbf15c05d34f277d
inesgn <ines.garmendia@gmail.com>
2015-03-17 15:17:33 +0100

    modificación para incorporación incremental de datos nuevos

--- r-scripts/serieA/serieA-escrapeo.R
+++ r-scripts/serieA/serieA-escrapeo.R
@@ -38,32 +38,60 @@ firstURL <- "http://www.congreso.es/portal/page/portal/Congreso/Congreso/Publica
 # Paso 1) Escrapeo firstURL (='directorio' Serie A)
 ###################################################
 
+#-------------------------------#
+#     Primera (y única) vez     #
+#-------------------------------#
 ## Crear un directorio de proyectos*URLS
 doc  <- htmlTreeParse(getURL(firstURL), encoding="UTF-8", useInternalNodes=TRUE)
 f1 <- getNodeSet(doc, '//*[@class="resultados_encontrados"]')
-length(f1) #134 documentos
+length(f1) #136 documentos)
 
 #llamamos a la función propia construir_dir_serieA
 proy_listA <- construir_dir_serieA(f1 = f1, proy_listA = proy_listA)
+#NOTA. hasta que mejoremos el escrapeo esto es inestable, algunas URLS no son correctas.
 
 #Guardar el directorio
 save(proy_listA, file = "dir-serieA.rd")
-# load("dir-serieA.rd") #para cargarlo
 
 proy_listA[[1]] #ej. primer proyecto de ley de la lista
+
+#----------------------------#
+#         Siguientes         #
+#----------------------------#
+#Cargamos el directorio con toda la serie hasta la fecha
+load("dir-serieA.rd")
+
+#escrapeamos para ver los nuevos
+doc  <- htmlTreeParse(getURL(firstURL), encoding="UTF-8", useInternalNodes=TRUE)
+f1 <- getNodeSet(doc, '//*[@class="resultados_encontrados"]')
+length(f1) #136 documentos)
+
+#creamos directorio actualizado, y contrastamos
+proy_listA_updated <- construir_dir_serieA(f1 = f1, proy_listA = proy_listA)
+
+proy_listA_pdte <- setdiff(x = proy_listA_updated, y = proy_listA)
+
+#guardamos pendientes
+save(proy_listA_pdte, file="abl.rd")
+#actualizamos directorio completo
+rm(proy_listA)
+proy_listA <- proy_listA_updated
+save(proy_listA, file = "dir-serieA.rd")
+rm(proy_listA_updated)
 
 #######################################################################
 # Paso 2) Escrapeo secURL (='directorio' de cada boletin de la Serie A)
 #######################################################################
 
-### Bucle FOR para descargar todos los boletines para los Proyectos de ley dados.
+### Bucle FOR para descargar todos los boletines para as Proposiciones de ley dadas
 
-### para probar solo descargamos los dos primeros
-
-length(proy_listA) #134 Proyectos
-for(i in 101:134){#i=90 para descargar uno de ellos; for(i in 1:length(proy_listA)) para descargar todos
+#-------------------------------#
+#     Primera (y única) vez     #
+#-------------------------------#
+length(proy_listA) #136 proyectos de ley en total
+for(i in 1:length(proy_listA)){#i=90 para descargar uno de ellos; for(i in 1:length(proy_listA)) para descargar todos
         secURL <- proy_listA[[i]]$url
-#         browseURL(secURL) #para comprobar
+        #   browseURL(secURL) #para comprobar
         #escrapeamos
         doc  <- htmlTreeParse(getURL(secURL), encoding="UTF-8", useInternalNodes=TRUE)
         f2 <- getNodeSet(doc, '//*[@class="resultados_encontrados"]')
@@ -81,7 +109,37 @@ for(i in 101:134){#i=90 para descargar uno de ellos; for(i in 1:length(proy_list
         if(length(bol_listA)>0 & length(bol_listA[[1]]$codigo)>0){
                 e <- str_extract(string = bol_listA[[1]]$codigo, pattern = "A-.*-")
                 filename <- paste0("dir-",substr(e, start=0,stop=nchar(e)-1),".rd")
-                save(bol_listA, file=filename)
+                if(!file.exists(filename)){ save(bol_listA, file=filename) }
+        }
+}
+
+#----------------------------#
+#         Siguientes         #
+#----------------------------#
+# Cargar la lista de boletines pendientes
+load("abl.rd") # se carga la lista proy_listA_pdte
+length(proy_listA_pdte) #pendientes de descarga
+for(i in 1:length(proy_listA_pdte)){#i=90 para descargar uno de ellos; for(i in 1:length(proy_listA)) para descargar todos
+        secURL <- proy_listA_pdte[[i]]$url
+        #   browseURL(secURL) #para comprobar
+        #escrapeamos
+        doc  <- htmlTreeParse(getURL(secURL), encoding="UTF-8", useInternalNodes=TRUE)
+        f2 <- getNodeSet(doc, '//*[@class="resultados_encontrados"]')
+        
+        ## almacenar todos los boletines del Proyecto de Ley en cuestión
+        #va guardando ficheros locales, si no se desea guardar fichero local ponemos guardarlocal=FALSE.
+        #         length(f2) #19 documentos
+        
+        #llamamos a la función propia construir_dir_bolA
+        #inicializamos la lista
+        bol_listA <- list()
+        #NOTA. Si guardarlocal = TRUE genera un fichero local que después se procesará.
+        bol_listA <- construir_dir_bolA(f2 = f2, guardarlocal = TRUE, bol_list = bol_listA)
+        #guardamos bol_listA para mantener un recuento
+        if(length(bol_listA)>0 & length(bol_listA[[1]]$codigo)>0){
+                e <- str_extract(string = bol_listA[[1]]$codigo, pattern = "A-.*-")
+                filename <- paste0("dir-",substr(e, start=0,stop=nchar(e)-1),".rd")
+                if(!file.exists(filename)){ save(bol_listA, file=filename) }
         }
 }
 
@@ -103,4 +161,3 @@ for(i in 101:134){#i=90 para descargar uno de ellos; for(i in 1:length(proy_list
 # ..$ url     : chr "http://www.congreso.es/portal/page/portal/Congreso/PopUpCGI?CMD=VERLST&CONF=BRSPUB.cnf&BASE=PU10&DOCS=1-1&FMT=PUWTXDTS.fmt&OPDE"| __truncated__
 # ..$ proc    : num 0
 # ..$ filename: chr "BOCG-A-1-1.rd"
-
