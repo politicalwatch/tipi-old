2f0a4445f7a395b5bc51b30d3c4aceec6144bd89
Alejandro Seguí <alesegdia@gmail.com>
2015-03-18 12:59:58 +0100

    serieA working with mongo-creds.

--- r-scripts/mongodb-creds.R
+++ r-scripts/mongodb-creds.R
@@ -1,13 +1,5 @@
 
-MONGODB_HOST     <- "localhost:27017"
+MONGODB_HOST     <- "ds043447.mongolab.com:43447"
 MONGODB_USERNAME <- "alesegdia"
 MONGODB_PASSWORD <- "1234"
-MONGODB_DATABASE <- "tipi_test"
-MONGODB_HOST_URL <- paste(MONGODB_HOST, MONGODB_DATABASE, "/")
-
-mongo <- mongo.create(host=MONGODB_HOST_URL)
-mongo.authenticate(mongo, username=MONGODB_USERNAME, password=MONGODB_PASSWORD, db=MONGODB_DATABASE)
-
-mongo_collection <- function(collection_name) {
-	return(paste0(MONGODB_DATABASE, collection_name, "."))
-}
+MONGODB_DATABASE <- "tipi_debug"
