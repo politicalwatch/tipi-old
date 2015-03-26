568b8cbc0c7ba316404c3da91e2b0efd1c922608
Alejandro Seguí <alesegdia@gmail.com>
2015-03-26 13:19:57 +0100

    Added incremental mongodb processing for serieB.

---
+++ r-scripts/serieB/serieB-procesamiento-incremental.R
@@ -0,0 +1,82 @@
+
+### Acerca de este script
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# Entrada: 
+## lista proy_listB con directorio actualizado Proyectos de Ley
+##        ---> fichero "dir-serieB.rd"
+## lista bol_listB con referencias a documentos para todos los Proyectos de Ley en proy_listB
+##        ---> ficheros del tipo "dir-A-X.rd"
+# Salida: ninguna; se alimenta bbdd mongo
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+	q <- mongo.bson.from.JSON(paste0('{ "bol":"123" }'))
+	a <- mongo.find(mongo, mongo_collection("serieA"), q)
+source("../common.R")
+source("../mongodb-conn.R")
+source("funciones-procesamiento.R")
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#      Procesamiento Boletines Serie A: Proyectos de Ley      #
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+load(paste0(GENERATED_BASE_DIR, "dir-serieB.rd")) # se carga proy_listB
+length(proy_listB)
+i=1
+names(proy_listB[[i]])
+
+# proy_listB[[i]]$codigo
+for(i in 1:length(proy_listB)){#i=1 #for(i in 1:length(proy_listB))
+
+	filename <- paste0(GENERATED_BASE_DIR, "dir-", proy_listB[[i]]$codigo, ".rd")
+	if(!file.exists(filename)){ next() }
+	load(filename) # se carga bol_listB
+	#         length(bol_listB) # hay 15 documentos asociados
+	# names(bol_listB[[i]]) #campos para cada tramite del Proyecto
+	# bol_listB[[i]]$tramite #trámite en cuestión
+
+	for(d in 1:length(bol_listB)){#d=1
+
+		q <- mongo.bson.from.JSON(paste0('{ "bol":"', bol_listB[[d]]$codigo, '" }'))
+		a <- mongo.find(mongo, mongo_collection("serieB"), q)
+		if(!mongo.cursor.next(a))
+		{
+			print("No estaba, añadiendo")
+			print(bol_listB[[d]]$codigo)
+			print("asd")
+			if(!file.exists(bol_listB[[d]]$filename)){ next() }
+			load(bol_listB[[d]]$filename) # carga lines, que contiene el texto a procesar
+
+			#Procesamiento idéntico para todos los B
+			lcont <- try(proc_serieB(lines, codigo = bol_listB[[d]]$codigo))
+			#caso de error al procesar: imprimir mensaje y enviar vacio.
+			if(class(lcont) == "try-error"){
+				lcont <- vector("list")
+				lcont$bol <- bol_listB[[d]]$codigo
+				print(paste("falla el boletin:", bol_listB[[d]]$codigo))
+				next()
+			}
+			#boletin procesado, enviar a MongoDB
+			#caso de boletin 'normal' (i.e. sin enmiendas)
+			if ( class(lcont) != "try-error" & length(lcont[[1]]) == 1 ) {
+				print(bol_listB[[d]]$codigo)
+
+				mongo.remove(mongo, mongo_collection("serieB"), criteria=list(bol=bol_listB[[d]]$codigo))
+				lcontb <- lapply(list(lcont), function(x) {return(mongo.bson.from.list(lcont))})
+				cat(" ", length(lcontb), "\n")
+				mongo.insert.batch(mongo, mongo_collection("serieB"), lcontb)
+			}
+			#caso de boletin con enmiendas. Ej B-157-5.
+			#el primer elemento seria a su vez una lista (con mas de 1 elemento), luego length()>1
+			if ( class(lcont) != "try-error" & length(lcont[[1]]) > 1 ){
+				mongo.remove(mongo, mongo_collection("serieB"), criteria=list(bol=bol_listB[[d]]$codigo))
+				print(bol_listB[[d]]$codigo)
+				lcontb <- lapply(lcont, function(x) {return(mongo.bson.from.list(x))})
+				mongo.insert.batch(mg, mongo_collection("serieB"), lcontb)
+			}
+		} else {
+			print("Ya estaba en mongo el boletin!")
+			print(bol_listB[[d]]$codigo)
+		}
+	}
+}
