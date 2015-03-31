16cf80394e2b244378496fef87d8f4d2de78d3b0
edipotrebol <edipotrebol@gmail.com>
2015-04-01 00:04:16 +0200

    Initialize scanner vizz schema

--- client/views/scanner/scanner.js
+++ client/views/scanner/scanner.js
@@ -11,33 +11,79 @@ Template.scanner.rendered = function() {
 
     // D3js example: https://raw.githubusercontent.com/Slava/d3-meteor-basic/master/client.js
 
-    var color = d3.scale.category10();
-    var bubble = d3.layout.pack()
-        .sort(null)
-        .size([30, 500])
-        .padding(1.5);
-        
-        var svg = d3.select("#vizz").append("svg")
-            .attr("width", 600)
-            .attr("height", 400)
-            .style("background-color", "#fff")
-            .attr("class","bubble");
-        
-        svg.selectAll("circle")
-            .data([32, 57, 112, 293,32, 57, 112, 293,32, 57, 112, 293,32, 57, 112, 293])
-            .enter().append("circle")
-            .attr("cy", function(d,i) {return Math.random()*200})
-            .attr("cx", function(d, i) {return Math.random()*200})
-            .attr("r", function(d) { return Math.sqrt(d); })
-            .style("fill", function (d) {return color(d)});
+    var margin = 20,
+    diameter = 500;
 
-        svg.selectAll("circle").append("svg:image")
-            .attr('x',0)
-            .attr('y',0)
-            .attr('width', 20)
-            .attr('height', 20)
-            .attr("xlink:href", "/public/images/franhealysvg2.svg");
+    var color = d3.scale.linear()
+        .domain([-1, 5])
+        .range(["hsl(82,60%,100%)", "hsl(228,30%,40%)"])
+        .interpolate(d3.interpolateHcl);
 
-        d3.select(self.frameElement).style("height", "50px");
+    var pack = d3.layout.pack()
+        .padding(2)
+        .size([diameter - margin, diameter - margin])
+        .value(function(d) { return d.size; })
+
+    var svg = d3.select("#vizz").append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .append("g")
+        .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+    d3.json("https://gist.githubusercontent.com/mbostock/7607535/raw/a05a94858375bd0ae023f6950a2b13fac5127637/flare.json", function(error, root) {
+      if (error) return console.error(error);
+
+      var focus = root,
+          nodes = pack.nodes(root),
+          view;
+
+      var circle = svg.selectAll("circle")
+          .data(nodes)
+        .enter().append("circle")
+          .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+          .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+          .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+
+      var text = svg.selectAll("text")
+          .data(nodes)
+        .enter().append("text")
+          .attr("class", "label")
+          .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+          .style("display", function(d) { return d.parent === root ? null : "none"; })
+          .text(function(d) { return d.name; });
+
+      var node = svg.selectAll("circle,text");
+
+      d3.select("body")
+          .style("background", color(-1))
+          .on("click", function() { zoom(root); });
+
+      zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+      function zoom(d) {
+        var focus0 = focus; focus = d;
+
+        var transition = d3.transition()
+            .duration(d3.event.altKey ? 7500 : 750)
+            .tween("zoom", function(d) {
+              var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+              return function(t) { zoomTo(i(t)); };
+            });
+
+        transition.selectAll("text")
+          .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+            .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+            .each("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+            .each("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+      }
+
+      function zoomTo(v) {
+        var k = diameter / v[2]; view = v;
+        node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+        circle.attr("r", function(d) { return d.r * k; });
+      }
+    });
+
+    d3.select(self.frameElement).style("height", diameter + "px");
 
 };
