579b6a5318bdf14b665b0dde25e09e6249e73050
Alejandro Seguí <alesegdia@gmail.com>
2015-03-31 13:22:35 +0200

    Modifying exported file name from proclines.csv to refs.csv.

--- client/views/refs/refsearch.js
+++ client/views/refs/refsearch.js
@@ -62,7 +62,7 @@ Template.refsearch.events({
 		var collection_data = Refs.find(query).fetch();
 		var data = json2csv(collection_data, true, true);
 		var blob = new Blob([data], {type: "text/csv;charset=utf-8"});
-		saveAs(blob, "proclines.csv");
+		saveAs(blob, "refs.csv");
 	}
   //
 });
