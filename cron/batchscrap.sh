7ac252cc0797d5852415d7f9900dfe33de33ace2
Alejandro Seguí <alesegdia@gmail.com>
2015-05-22 14:31:35 +0200

    added cron script

---
+++ cron/batchscrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+BASE_DIR=/home/razieliyo/tipi/
+
+function scrapnproc {
+	cd "$BASE_DIR"/r-scripts/"$1"
+	if [ $3 = "sp" ]
+	then
+		Rscript $2-escrapeo-unif.R
+		Rscript $2-procesamiento-incremental.R
+	elif [ $3 = "p" ]
+	then
+		Rscript $2-procesamiento-incremental.R
+	fi
+}
+
+scrapnproc serieA serieA sp
+scrapnproc serieB serieB sp
+scrapnproc serieD serieD sp
+scrapnproc Diarios-Pleno DS-Comisiones sp
+scrapnproc Diarios-Pleno DS-Pleno sp
