858e921941bc39de3fe5b849594dc4e562d7dd7a
Alejandro Seguí <alesegdia@gmail.com>
2015-05-22 12:17:25 +0200

    adding error file writing function

--- r-scripts/common.R
+++ r-scripts/common.R
@@ -2,6 +2,8 @@ options(stringsAsFactors=FALSE)
 
 GENERATED_BASE_DIR <- "gendir/"
 
+ERROR_LOG_FILE <- "/home/razieliyo/Rtipi_error.log"
+
 check_in_sublists_index <- function(list2d, to_check) {
 	if( length(list2d) > 0 ) {
 		for(i in 1:length(list2d)){
@@ -11,4 +13,16 @@ check_in_sublists_index <- function(list2d, to_check) {
 		}
 	}
 	return(FALSE)
+}
+
+check_in_mongo <- function(json_query) {
+	q <- mongo.bson.from.JSON(json_query )
+	a <- mongo.find(mongo, mongo_collection(collection_name), q)
+	return(mongo.cursor.next(a))
+}
+
+write_error_log <- function(error) {
+	file <- file(ERROR_LOG_FILE)
+	writeLines(error, file)
+	close(file)
 }
