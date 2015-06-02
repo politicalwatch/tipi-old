a49b49d9f993a238e3d6e0f0534649975398b934
edipotrebol <edipotrebol@gmail.com>
2015-06-02 18:49:30 +0200

    Solving fix in dicts view with IDs

--- client/views/dicts/dicts.js
+++ client/views/dicts/dicts.js
@@ -14,9 +14,9 @@ Template.dicts.helpers({
 				{ key: 'dictgroup', label: 'Grupo'},
 				{ key: 'acciones', label: 'Acciones', 
 				    fn: function(val, obj) {
-					   var actstr = '<a href="/dicts/'+ obj._id._str + '"><span class="label label-info"><i class="fa fa-eye"></i></span></a>&nbsp;';
+					   var actstr = '<a href="/dicts/'+ obj._id + '"><span class="label label-info"><i class="fa fa-eye"></i></span></a>&nbsp;';
 					   if (Roles.userIsInRole(Meteor.user(), ["admin"])) {
-					       actstr += '<a href=\'/admin/Dicts/ObjectID("'+ obj._id._str + '")/edit\'><span class="label label-warning"><i class="fa fa-pencil"></i></span></a>&nbsp;';
+					       actstr += '<a href=\'/admin/Dicts/'+ obj._id + '/edit\'><span class="label label-warning"><i class="fa fa-pencil"></i></span></a>&nbsp;';
 					   }
 					   return Spacebars.SafeString(actstr);
 				    }
