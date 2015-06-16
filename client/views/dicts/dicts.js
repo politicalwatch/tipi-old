4295737d442ba5660d7adb400e4c37f31ba3a0f7
pr3ssh <pablo.martin@enreda.coop>
2015-06-16 18:33:13 +0200

    Allow users to check dicts + Export users by dicts

--- client/views/dicts/dicts.js
+++ client/views/dicts/dicts.js
@@ -17,6 +17,7 @@ Template.dicts.helpers({
 					   var actstr = '<a href="/dicts/'+ obj._id + '"><span class="label label-info"><i class="fa fa-eye"></i></span></a>&nbsp;';
 					   if (Roles.userIsInRole(Meteor.user(), ["admin"])) {
 					       actstr += '<a href=\'/admin/Dicts/'+ obj._id + '/edit\'><span class="label label-warning"><i class="fa fa-pencil"></i></span></a>&nbsp;';
+                           actstr += '<a class="exportusersbydict" id="'+obj._id+'" href=\'#\'><span class="label label-default"><i class="fa fa-users"></i></span></a>&nbsp;';
 					   }
 					   return Spacebars.SafeString(actstr);
 				    }
@@ -37,5 +38,20 @@ Template.dicts.events({
 		e.preventDefault();
 		Router.go('/dicts/new');
 	},
-  //
+    'click .exportusersbydict': function(e) {
+        dictionary = Dicts.findOne(e.currentTarget.id);
+        var collection_data = Meteor.users.find({'profile.dicts': dictionary.dict}, {fields: {username: 1, 'profile.firstname': 1, 'profile.lastname': 1, 'emails': 1}}).fetch();
+        var collection_data_mod = [];
+        _.each(collection_data, function(col) {
+            u = {}
+            u['username'] = col.username;
+            u['firstname'] = col.profile.firstname;
+            u['lastname'] = col.profile.lastname;
+            u['email'] = col.emails[0].address;
+            collection_data_mod.push(u);
+        });
+        var data = json2csv(collection_data_mod, true, true);
+        var blob = new Blob([data], {type: "text/csv;charset=utf-8"});
+        saveAs(blob, dictionary.dict.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-') + "_users.csv");
+    }
 });
--- client/views/users/profile_edit.js
+++ client/views/users/profile_edit.js
@@ -1,5 +1,23 @@
 Template.profileEdit.helpers({
-    userSchema: function () {
+    userSchema: function() {
         return Schema.User;
+    },
+    otherDicts: function() {
+        myDicts = Meteor.users.findOne({}, {fields: {'profile.dicts': 1}});
+        allDicts = Dicts.find({}, {fields: {dict: 1}}).fetch();
+        dicts = [];
+        _.each(allDicts, function(d) {
+            dicts.push(d.dict);
+        });
+        return _.difference(dicts, myDicts.profile.dicts);
     }
 });
+
+Template.profileEdit.events = {
+    'click .btn-add-dict': function(e) {
+        Meteor.call('addDictToUser', $(e.target).attr('user'), $(e.target).attr('dict'));
+    },
+    'click .btn-remove-dict': function(e) {
+        Meteor.call('removeDictToUser', $(e.target).attr('user'), $(e.target).attr('dict'));
+    }
+};
