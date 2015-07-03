714587bd38acbcbf1c8c99b090b0e8463d2999bb
inesgn <ines.garmendia@gmail.com>
2015-07-03 16:22:51 +0200

    Modificado formato fechas

--- r-scripts/Diarios-Pleno/DS-Pleno-procesamiento-incremental.R
+++ r-scripts/Diarios-Pleno/DS-Pleno-procesamiento-incremental.R
@@ -72,7 +72,7 @@ for(i in 1:length(l)){ #i=8 #i in 1:length(listos_mongo)
 		#Añadir url
 		lcont$url <- paste0("http://www.congreso.es", abl[num, "url"])
 		#Añadir fecha
-		lcont$fecha <- abl[num, "date"]
+		lcont$fecha <- as.POSIXct(abl[num, "date"], tz="CET")
 		print(paste("falla el boletin:", num))
 		next()
 		write_error_log("DS-Pleno", paste0("boletin numero ",num), "procesamiento erróneo")
@@ -101,7 +101,7 @@ for(i in 1:length(l)){ #i=8 #i in 1:length(listos_mongo)
 				lcont2 <- list()
 				lcont2[[1]] <- crearCampoAutor(lcont[[k]])
 				lcont2[[1]]$url <- paste0("http://www.congreso.es", abl[num, "url"]) 
-				lcont2[[1]]$fecha <- abl[num, "date"]
+				lcont2[[1]]$fecha <- as.POSIXct(abl[num, "date"], tz="CET")
 				lcont2[[1]]$origen <- "diariosPD"
 				lcontb <- lapply(lcont2, function(x) {return(mongo.bson.from.list(x))})
 				mongo.insert.batch(mongo, mongo_collection("diariosPD"), lcontb)
