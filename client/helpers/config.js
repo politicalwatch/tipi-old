de36f7be86f8a3cda188b196221387235a25d894
pr3ssh <pablo.martin@enreda.coop>
2015-07-18 12:42:54 +0200

    Loading page styled!

---
+++ client/helpers/config.js
@@ -0,0 +1,16 @@
+Meteor.Spinner.options = {
+    lines: 10,
+    length: 1,
+    width: 8,
+    radius: 20,
+    corners: 1,
+    rotate: 0,
+    direction: 1,
+    color: '#fbc239',
+    speed: 1,
+    trail: 60,
+    shadow: true,
+    hwaccel: false,
+    className: 'spinner',
+    zIndex: 2e9
+}
