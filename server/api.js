3a14acc83ae51fa002e323bfb852287d578bde4b
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-09-21 18:24:48 +0200

    added endpoints for stats

--- server/api.js
+++ server/api.js
@@ -10,7 +10,7 @@ if (Meteor.isServer) {
     useDefaultAuth: false,
     prettyJson: true
   });
-
+//TIPIS
 Api.addRoute('tipis/', {
     get: function () {
     	var limit =  parseInt(this.queryParams.limit) || 10
@@ -20,7 +20,7 @@ Api.addRoute('tipis/', {
     	if (this.queryParams.dict){
     		dict = Dicts.find({slug: this.queryParams.dict, group: "tipi"},{fields: {name: 1}}).fetch();
     		if (dict.length>0) {
-    			return Tipis.find({dicts:dict[0].dict}, {limit: limit, skip : pag }).fetch();
+    			return Iniciativas.find({'is.tipi':true,'dicts.tipi':dict[0].name}, {limit: limit, skip : pag }).fetch();
     		} else {
     			return {
 			    statusCode: 404,
@@ -28,7 +28,8 @@ Api.addRoute('tipis/', {
 			}
 		}			 
     	} else {
-    	    return Tipis.find({}, {limit: limit, skip: pag }).fetch();
+            
+    	    return Iniciativas.find({}, {limit: limit, skip: pag }).fetch();
     	}
     }
 });
@@ -38,7 +39,7 @@ Api.addRoute('tipis/', {
 Api.addRoute('tipis/:_id', {
 	get: function () {
             var oid = new Meteor.Collection.ObjectID(this.urlParams._id);
-            var tipi=Tipis.findOne(oid);
+            var tipi=Iniciativas.findOne(oid);
             if (tipi){
                 return tipi
             } else {
@@ -49,5 +50,150 @@ Api.addRoute('tipis/:_id', {
             }
 	}
 });
+
+//STATS
+//overall
+Api.addRoute('stats/overall', {
+    get: function () {
+        return TipiStats.find({},{fields:{overall:1,'_id': false}}).fetch()
+
+    }
+           
+});
+
+
+Api.addRoute('stats/overall/:slug', {
+    get: function () {
+        dict = Dicts.find({slug: this.urlParams.slug, group: "tipi"},{fields: {name: 1}}).fetch();
+        if (dict.length>0){
+            stat = TipiStats.find({overall:{$elemMatch:{_id:dict[0].name}}},{fields:{overall:1,'_id': false}}).fetch()
+            if (stat){
+                return stat
+            } else {
+                return {
+                        statusCode: 404,
+                        body: []
+                        }
+            }
+        }else{
+            return {
+                    statusCode: 404,
+                    body: []
+                    }
+        }
+
+
+    }
+           
+});
+
+
+//bydeputies
+Api.addRoute('stats/bydeputies', {
+    get: function () {
+        return TipiStats.find({},{fields:{bydeputies:1,'_id': false}}).fetch()
+
+    }
+           
+});
+
+
+Api.addRoute('stats/bydeputies/:slug', {
+    get: function () {
+        dict = Dicts.find({slug: this.urlParams.slug, group: "tipi"},{fields: {name: 1}}).fetch();
+        if (dict.length>0){
+            stat = TipiStats.find({bydeputies:{$elemMatch:{_id:dict[0].name}}},{fields:{bydeputies:1,'_id': false}}).fetch()
+            if (stat){
+                return stat
+            } else {
+                return {
+                        statusCode: 404,
+                        body: []
+                        }
+            }
+        }else{
+            return {
+                    statusCode: 404,
+                    body: []
+                    }
+        }
+
+
+    }
+           
+});
+
+//bygroups
+Api.addRoute('stats/bygroups', {
+    get: function () {
+        return TipiStats.find({},{fields:{bygroups:1,'_id': false}}).fetch()
+
+    }
+           
+});
+
+
+Api.addRoute('stats/bygroups/:slug', {
+    get: function () {
+        dict = Dicts.find({slug: this.urlParams.slug, group: "tipi"},{fields: {name: 1}}).fetch();
+        if (dict.length>0){
+            stat = TipiStats.find({bygroups:{$elemMatch:{_id:dict[0].name}}},{fields:{bygroups:1,'_id': false}}).fetch()
+            if (stat){
+                return stat
+            } else {
+                return {
+                        statusCode: 404,
+                        body: []
+                        }
+            }
+        }else{
+            return {
+                    statusCode: 404,
+                    body: []
+                    }
+        }
+
+
+    }
+           
+});
+
+
+//latest
+Api.addRoute('stats/latest', {
+    get: function () {
+        return TipiStats.find({},{fields:{latest:1,'_id': false}}).fetch()
+
+    }
+           
+});
+
+
+
+
+Api.addRoute('stats/latest/:slug', {
+    get: function () {
+        dict = Dicts.find({slug: this.urlParams.slug, group: "tipi"},{fields: {name: 1}}).fetch();
+        if (dict.length>0){
+            stat = TipiStats.find({latest:{$elemMatch:{_id:dict[0].name}}},{fields:{latest:1,'_id': false}}).fetch()
+            if (stat){
+                return stat
+            } else {
+                return {
+                        statusCode: 404,
+                        body: []
+                        }
+            }
+        }else{
+            return {
+                    statusCode: 404,
+                    body: []
+                    }
+        }
+
+    }
+           
+});
+
 
 }; //end
