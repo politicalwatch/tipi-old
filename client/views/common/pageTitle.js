aab8928cab824c29518dec58948ed363bd495b65
pr3ssh <pablo.martin@enreda.coop>
2016-09-23 09:28:48 +0200

    Changes in stats views

--- client/views/common/pageTitle.js
+++ client/views/common/pageTitle.js
@@ -4,6 +4,8 @@ var pagesWithoutTitle = [
 
 Template.pageTitle.helpers({
     hasTitle: function() {
+        // console.log(_.indexOf(pagesWithoutTitle, window.location.pathname));
+        // console.log(window.location.pathname);
         if (_.indexOf(pagesWithoutTitle, window.location.pathname) != -1) {
             return false;
         } else {
