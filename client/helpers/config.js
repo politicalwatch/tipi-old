2f6ad66694d34c98a96ba86a8b6335e775d5dcda
pr3ssh <pablo.martin@enreda.coop>
2016-09-26 20:33:21 +0200

    Change loading page

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -6,10 +6,10 @@ Meteor.Spinner.options = {
     corners: 1,
     rotate: 0,
     direction: 1,
-    color: '#fbc239',
+    color: '#222',
     speed: 1,
     trail: 60,
-    shadow: true,
+    shadow: false,
     hwaccel: false,
     className: 'spinner',
     zIndex: 2e9
